2e65e30f078c3ab57ee7849adba257121ab10ff7
Moazam Ali <contactmoazam@gmail.com>
2026-04-13 12:49:46 +0500

    lesson 1

---
+++ lesson-1/node-chat-lesson/index.js
@@ -0,0 +1,22 @@
+var app = require('express')();
+var server = require('http').Server(app);
+var io = require('socket.io')(server);
+
+server.listen(3000,()=>{
+    console.log("connecting to server");
+});
+app.get('/',function(req,res){
+    res.sendFile(__dirname+"/index.html");
+});
+
+io.on('connection',(socket)=>{
+    console.log("a connection was made.");
+    socket.on('chat.message',function(message){
+        console.log('New Message :'+message);
+        io.emit('chat.message',message);
+    });
+    socket.on('disconnect',function(){
+          console.log('New Message :"User has Disconnected" ');
+        io.emit('chat.message',"User has Disconnected");
+    });
+})
